Implemented samme.r h_k method of classification and fixed double softmax bug

boosted_classifier.py
@@ -5,45 +5,42 @@ import sonnet as snt
 class NaiveBoostedClassifier(snt.AbstractModule):
     def __init__(self, num_blocks, class_num, name='boosted_classifier'):
         super(NaiveBoostedClassifier, self).__init__(name=name)
+        self._class_num = class_num
 
         self._blocks = []
         self._classifiers = []
-        self._alphas = []
         with self._enter_variable_scope():
             self._entry_layer = snt.Conv2D(32, 3)
             for i in range(num_blocks):
                 self._blocks.append(
                     snt.Sequential([
-                        snt.Residual(snt.Conv2D(32, 3)),
-                        snt.Residual(snt.Conv2D(32, 3)),
-                        snt.Residual(snt.Conv2D(32, 3))
+                        snt.Residual(snt.Conv2D(32, 3)), tf.nn.elu,
+                        snt.Residual(snt.Conv2D(32, 3)), tf.nn.elu,
+                        snt.Residual(snt.Conv2D(32, 3)), tf.nn.elu
                     ]))
                 self._classifiers.append(
                     snt.Sequential(
-                        [tf.layers.Flatten(),
+                        [snt.Conv2D(3,3),
+                         tf.layers.Flatten(),
                          snt.Linear(class_num)]))
-                self._alphas.append(
-                    tf.get_variable(
-                        'alpha_{}'.format(i),
-                        initializer=tf.constant(1.),
-                        dtype=tf.float32,
-                        trainable=False),
-                )  # THIS SHOULD BE UPDATED TO ADABOOST NUM
 
     def _build(self, inputs):
         logits = []
-        contributions = []
+        h_ks = []
         x = self._entry_layer(inputs)
         for i, _ in enumerate(self._blocks):
             x = self._blocks[i](x)
             c = self._classifiers[i](x)
             logits.append(c)
-            contributions.append(self._alphas[i] * c)
+            probs = tf.nn.softmax(c)
+            hk_inner_prod = tf.constant(
+                (1 / self._class_num),
+                dtype=tf.float32,
+                shape=(self._class_num, self._class_num))
+            hk_inner_prod = tf.matrix_set_diag(hk_inner_prod,
+                                               tf.ones([self._class_num]))
+            block_h_k = (1 / self._class_num) * tf.matmul(probs, hk_inner_prod)
+            h_ks.append(block_h_k)
 
-    #p_k = tf.reduce_sum(label_ph * classification, axis=1)
-    #p_other = (tf.reduce_sum(classification, axis=1) - p_k) / class_num
-    #h_k = (class_num - 1)(tf.log(p_k) - p_other)
-
-        final_classification = tf.accumulate_n(
-            contributions) / tf.accumulate_n(self._alphas)
+        final_classification = tf.accumulate_n(h_ks)
         return final_classification, logits

train.py
@@ -12,6 +12,7 @@ import util
 parser = argparse.ArgumentParser(description='Self-Boosting Network Training.')
 parser.add_argument('--batch_size', type=int, default=32)
 parser.add_argument('--epochs', type=int, default=100)
+parser.add_argument('--blocks', type=int, default=5)
 args = parser.parse_args()
 
 # load the data
@@ -31,22 +32,29 @@ label_ph = tf.placeholder(tf.int32, shape=label_shape)  # should be one-hot
 final_classification, weak_logits = classifier(data_ph)
 weak_classifications = [tf.nn.softmax(logits) for logits in weak_logits]
 
+correct_prop = tf.count_nonzero(
+    tf.equal(tf.argmax(final_classification, 1), tf.argmax(label_ph, 1)),
+    dtype=tf.float32) / tf.constant(
+        args.batch_size, dtype=tf.float32)
+
+
 def feed_dict_fn():
     data, labels = next(train_gen)
     return {data_ph: data, label_ph: labels}
+
 
 # calculate boosting weights
 weights = tf.constant(
     1. / args.batch_size, dtype=tf.float32, shape=(args.batch_size, ))
 losses = []
-for classification in weak_classifications:
+for i, classification in enumerate(weak_classifications):
     scale = -(float(class_num) - 1.) / float(class_num)
     weights = weights * tf.reduce_sum(
         tf.exp(scale *
                (tf.to_float(label_ph) * tf.log(classification + 1e-10))),
         axis=1)
     losses.append(weights * tf.nn.sparse_softmax_cross_entropy_with_logits(
-        labels=tf.argmax(label_ph, axis=1), logits=classification))
+        labels=tf.argmax(label_ph, axis=1), logits=weak_logits[i]))
 
 # calculate gradients
 optimizer = tf.train.AdamOptimizer()
@@ -67,11 +75,12 @@ with tf.Session(config=config) as session:
     session.run(tf.global_variables_initializer())
     for epoch in range(args.epochs):
         print("EPOCH {}".format(epoch))
-        norms = util.run_epoch_ops(
+        norms, acc = util.run_epoch_ops(
             session,
             train_data.shape[0] // args.batch_size,
             silent_ops=[train_op],
-            verbose_ops=[out_grads],
+            verbose_ops=[out_grads, correct_prop],
             feed_dict_fn=feed_dict_fn,
             verbose=True)
         print(norms)
+        print(acc)