000c77cd683f777ed6ffab2b29f87623c1824a77
Quint <pquint@cse.unl.edu>
2018-06-04 15:28:04 -0500

    Added code for unboosted classification to use as a baseline

--- networks.py
+++ networks.py
@@ -14,3 +14,16 @@ class ResidualConvBlock(snt.AbstractModule):
 
     def _build(self, inputs):
         return self._block(inputs)
+
+class ResidualClassifier(snt.AbstractModule):
+    def __init__(self, num_blocks, class_num, name='residual_classifier'):
+        super(ResidualClassifier, self).__init__(name=name)
+        with self._enter_variable_scope():
+            entry_layer = snt.Sequential(
+                [snt.Conv2D(32, 3, name='entry_conv2d'), tf.nn.elu])
+            blocks = snt.Sequential([ResidualConvBlock(32) for _ in range(num_blocks)])
+            classifier = snt.Linear(class_num)
+            self._net = snt.Sequential([entry_layer, blocks, tf.layers.Flatten(), classifier])
+
+    def _build(self, inputs):
+        return self._net(inputs)
---
+++ train_unboosted.py
@@ -0,0 +1,70 @@
+import argparse
+from functools import reduce
+
+import numpy as np
+import sonnet as snt
+import tensorflow as tf
+
+import networks
+import data.data as data
+import util
+
+parser = argparse.ArgumentParser(description='Self-Boosting Network Training.')
+parser.add_argument('--batch_size', type=int, default=32)
+parser.add_argument('--epochs', type=int, default=100)
+parser.add_argument('--blocks', type=int, default=5)
+args = parser.parse_args()
+
+# load the data
+train_data, train_labels, _, _ = data.load_cifar10('./data/')
+data_shape = (args.batch_size, ) + train_data.shape[1:]
+label_shape = (args.batch_size, ) + train_labels.shape[1:]
+class_num = train_labels.shape[1]
+train_gen = data.parallel_data_generator([train_data, train_labels],
+                                         args.batch_size)
+
+# define the model
+classifier = networks.ResidualClassifier(args.blocks, 10)
+
+# build model
+data_ph = tf.placeholder(tf.float32, shape=data_shape)
+label_ph = tf.placeholder(tf.int32, shape=label_shape)  # should be one-hot
+final_classification = classifier(data_ph)
+loss = tf.nn.sparse_softmax_cross_entropy_with_logits(
+    labels=tf.argmax(label_ph, axis=1), logits=final_classification)
+
+class_rate_fn = lambda a: tf.count_nonzero(
+    tf.equal(tf.argmax(a, 1), tf.argmax(label_ph, 1)),
+    dtype=tf.float32) / tf.constant(
+        args.batch_size, dtype=tf.float32)
+correct_final_prop = class_rate_fn(final_classification)
+
+
+def feed_dict_fn():
+    data, labels = next(train_gen)
+    return {data_ph: data, label_ph: labels}
+
+
+# calculate gradients
+optimizer = tf.train.AdamOptimizer()
+train_op = optimizer.minimize(loss)
+
+print("Trainable Parameters: {}".format(
+    np.sum(
+        [np.prod(v.get_shape().as_list()) for v in tf.trainable_variables()])))
+
+# initialize session and train
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+with tf.Session(config=config) as session:
+    session.run(tf.global_variables_initializer())
+    for epoch in range(args.epochs):
+        print("EPOCH {}".format(epoch))
+        acc = util.run_epoch_ops(
+            session,
+            train_data.shape[0] // args.batch_size,
+            silent_ops=[train_op],
+            verbose_ops=[correct_final_prop],
+            feed_dict_fn=feed_dict_fn,
+            verbose=True)
+        print("Accuracy: " + str(acc))
