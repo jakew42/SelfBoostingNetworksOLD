0f84cf6be7a01581451449bf80d2905282aa3eb0
Quint <DrKwint@gmail.com>
2018-07-12 12:11:37 -0500

    Drkwint/metrics (#15)
    * First changes to run_ops and adding visualization of weak learner errors
    
    * Finalized the histogram code for plotting weak learner accuracy
    
    * Moved histograms to logs directory, cleaned code with yapf, and added docs
    
    * Made metrics and logging optional, moved histogramming out to another function, and changed run_epoch to take a dict
    
    * Change flag arguments to not require True
    Print metrics to file.
    Cleaned run_epoch_ops code a bit
    
    * Setup the ability to choose classes (#12)
    
    * Setup the ability to choose classes
    
    * replaced generator with reasonable numpy in label idx selection
    
    * Drkwint/strategies (#13)
    
    * Non-building commit to save work
    
    * tested with samme.r strat and working
    
    * Fixed jake-kuns experimental approach strategy
    
    * final yapf
    
    * Added per weak classifier gradient logging and cleaned up gradient code a bit
    
    * Update train.py
    
    * fixed a bug with gradient calculations
    
    * non-building work on adding confusion matrices
    
    * Finalized and tested per weak classifier confusion matrices
    
    * fixed a bug with confusion matrices
    
    * Update train.py

--- train.py
+++ train.py
@@ -27,19 +27,33 @@ parser.add_argument('--log_dir', type=str, default='./logs/')
 metrics_args = parser.add_argument_group('Metrics')
 metrics_args.add_argument('--gradient_norms', action="store_true")
 metrics_args.add_argument('--weak_classifier_covar', action="store_true")
-histogram_args = parser.add_argument_group('Histograms')
+histogram_args = parser.add_argument_group('Plots')
 histogram_args.add_argument('--weak_classifier_rate', action="store_true")
+metrics_args.add_argument('--weak_classifier_conf_matrix', action="store_true")
 args = parser.parse_args()
 
+CIFAR10_CLASS_NAMES = [
+    'airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse',
+    'ship', 'truck'
+]
+
+# Setup log directory and log files
 if not os.path.exists(args.log_dir):
     os.makedirs(args.log_dir)
 if args.gradient_norms:
     with open(os.path.join(args.log_dir, 'grad_norms.csv'), 'w') as csv:
-        csv.write('pre_clip_grad_vals, post_clip_grad_vals\n')
+        csv.write('pre_clip_grad_vals, post_clip_grad_vals,{}\n'.format(
+            ','.join([
+                'pre_clip_wc_{}_grad_vals'.format(i)
+                for i in range(args.blocks)
+            ])))
 if args.weak_classifier_covar:
     with open(os.path.join(args.log_dir, 'weak_learner_covariance.csv'),
               'w') as csv:
         csv.write('weak_classifier_covariance')
+if args.weak_classifier_conf_matrix and not os.path.exists(
+        os.path.join(args.log_dir, 'wc_confusion_matrices')):
+    os.makedirs(os.path.join(args.log_dir, 'wc_confusion_matrices'))
 
 block_dict = {
     'IdentityBlock': blocks.IdentityBlock,
@@ -96,6 +110,14 @@ data_ph = tf.placeholder(tf.float32, shape=data_shape)
 label_ph = tf.placeholder(tf.int32, shape=label_shape)  # should be one-hot
 final_classification, weak_logits = classifier(data_ph)
 weak_classifications = [tf.nn.softmax(logits) for logits in weak_logits]
+wc_confusion_matrices = [
+    tf.confusion_matrix(
+        tf.argmax(label_ph, axis=1),
+        tf.argmax(wl, axis=1),
+        num_classes=class_num,
+        dtype=tf.int32,
+    ) for wl in weak_logits
+]
 
 class_rate_fn = lambda a: tf.count_nonzero(
     tf.equal(tf.argmax(a, 1), tf.argmax(label_ph, 1)),
@@ -131,14 +153,23 @@ for i, classification in enumerate(weak_classifications):
 optimizer = tf.train.AdamOptimizer()
 grads_and_vars = [optimizer.compute_gradients(l) for l in weighted_losses]
 vars = list(zip(*grads_and_vars[0]))[1]
-grads = [[v if v is not None else 0 for v in list(zip(*gv))[0]]
+grads = [[v if v is not None else 0. for v in list(zip(*gv))[0]]
          for gv in grads_and_vars]
-pre_clip_grads = tf.global_norm([g for g in grads[0] if g != 0])
-grads = [sum(gs) for gs in list(zip(*grads))]
-update = list(zip(grads, vars))
-capped_gvs = [(tf.clip_by_value(grad, -1., 1.), var) for grad, var in update]
-post_clip_grads = tf.global_norm([g for g in capped_gvs[0] if g != 0])
-train_op = optimizer.apply_gradients(capped_gvs)
+total_grads = [sum(gs) for gs in list(zip(*grads))]
+clipped_grads = [tf.clip_by_value(grad, -1., 1.) for grad in total_grads]
+final_grads_and_vars = list(zip(clipped_grads, vars))
+train_op = optimizer.apply_gradients(final_grads_and_vars)
+
+pre_clip_wc_grads = [tf.global_norm(g) for g in grads]
+pre_clip_global_grads = tf.global_norm(total_grads)
+post_clip_global_grads = tf.global_norm(clipped_grads)
+
+# if the voting strategy has an update fn, use it
+# I, for one, welcome our new duck typing overlords
+if hasattr(classifier.voting_strategy, 'update'):
+    voting_strategy_update_op = classifier.voting_strategy.update(
+        weak_logits, label_ph)
+    train_op = tf.group(train_op, voting_strategy_update_op)
 
 # if the voting strategy has an update fn, use it
 # I, for one, welcome our new duck typing overlords
@@ -156,8 +187,11 @@ verbose_ops_dict['weak_classifiers'] = weak_classifications
 verbose_ops_dict['correct_weak_props'] = correct_weak_props
 verbose_ops_dict['correct_final_prop'] = correct_final_prop
 if args.gradient_norms:
-    verbose_ops_dict['pre_clip_grads'] = pre_clip_grads
-    verbose_ops_dict['post_clip_grads'] = post_clip_grads
+    verbose_ops_dict['pre_clip_global_grads'] = pre_clip_global_grads
+    verbose_ops_dict['post_clip_global_grads'] = post_clip_global_grads
+    verbose_ops_dict['pre_clip_wc_grads'] = pre_clip_wc_grads
+if args.weak_classifier_conf_matrix:
+    verbose_ops_dict['wc_conf_matrices'] = wc_confusion_matrices
 
 # initialize session and train
 config = tf.ConfigProto()
@@ -184,14 +218,20 @@ with tf.Session(config=config) as session:
         print("Weak Classifier Accuracies: " + str(mean_weak_acc_vals))
 
         if args.gradient_norms:
-            pre_clip_grad_vals = outs['pre_clip_grads']
-            mean_pre_clip_grad_vals = np.mean(pre_clip_grad_vals)
-            post_clip_grad_vals = outs['post_clip_grads']
-            mean_post_clip_grad_vals = np.mean(post_clip_grad_vals)
+            pre_clip_global_grad_vals = outs['pre_clip_global_grads']
+            mean_pre_clip_global_grad_vals = np.mean(pre_clip_global_grad_vals)
+            post_clip_global_grad_vals = outs['post_clip_global_grads']
+            mean_post_clip_global_grad_vals = np.mean(
+                post_clip_global_grad_vals)
+            pre_clip_wc_grad_vals = outs['pre_clip_wc_grads']
+            mean_pre_clip_wc_grad_vals = np.mean(pre_clip_wc_grad_vals, axis=0)
             with open(os.path.join(args.log_dir, 'grad_norms.csv'),
                       'a') as csv:
-                csv.write('{},{}\n'.format(mean_pre_clip_grad_vals,
-                                           mean_post_clip_grad_vals))
+                csv.write('{},{},{}\n'.format(
+                    mean_pre_clip_global_grad_vals,
+                    mean_post_clip_global_grad_vals, ','.join([
+                        '{}'.format(val) for val in mean_pre_clip_wc_grad_vals
+                    ])))
 
         if args.weak_classifier_covar:
             weak_classification_vals = np.reshape(
@@ -206,7 +246,7 @@ with tf.Session(config=config) as session:
                     'a') as csv:
                 csv.write('{}\n'.format(mean_weak_classification_covar_vals))
 
-        # histograms
+        # plots
         if args.weak_classifier_rate:
             util.plot_parallel_histograms(
                 values=np.swapaxes(weak_acc_vals, 0, 1),
@@ -216,3 +256,15 @@ with tf.Session(config=config) as session:
                 hist_title='Weak Learner',
                 xlabel='Correct Rate',
                 ylabel='Minibatches')
+
+        if args.weak_classifier_conf_matrix:
+            wc_conf_matrix_vals = np.sum(outs['wc_conf_matrices'], axis=0)
+            [
+                util.plot_confusion_matrix(
+                    cm,
+                    CIFAR10_CLASS_NAMES,
+                    title='confusion_matrix_epoch_{}_wc_{}'.format(epoch, i),
+                    log_dir=os.path.join(args.log_dir,
+                                         'wc_confusion_matrices'))
+                for i, cm in enumerate(wc_conf_matrix_vals)
+            ]
--- util.py
+++ util.py
@@ -1,5 +1,6 @@
 import os
 
+import itertools
 import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
@@ -78,3 +79,35 @@ def histogram(x, ax, num_bins, title, xlabel, ylabel):
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
     ax.set_title('{}: $\mu={:.3f}$, $\sigma={:.3f}$'.format(title, mu, sigma))
+
+def plot_confusion_matrix(cm, classes,
+                          title,
+                          log_dir,
+                          normalize=True,
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.savefig(os.path.join(log_dir, title + '.png'))
+    plt.clf()
